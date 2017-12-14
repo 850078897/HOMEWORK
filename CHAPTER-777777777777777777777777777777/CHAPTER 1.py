bf749d28d4ba2137aa36615b0d5f98592bc63b6d
850078897 <a850078897@gmail.com>
2017-12-14 14:33:06 -0500

    666
    666

--- CHAPTER-777777777777777777777777777777/CHAPTER 1.py
+++ CHAPTER-777777777777777777777777777777/CHAPTER 1.py
@@ -1,9 +1,21 @@
-mylist = [0,2,3,7,11,12]
-def count_odd(mylist):
+mylist = [0,2,3,7,11,12,14,20]
+def count_odd(list):
     sb = 0
-    for i in mylist:
+    for i in list:
         if i % 2 == 1:
             sb += 1
-    print(sb)
+    return sb
 
-def count_even(mylist)
+def count_even(list):
+    sbeven= 0
+    for i in list:
+        if i % 2 == 0:
+            sbeven += i
+    return sbeven
+
+def count_negative(list):
+    sbnegative = 0
+    for i in list:
+        if i  < 0:
+            sbnegative += i
+    return sbnegative
