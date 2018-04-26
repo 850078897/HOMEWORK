39cd7aaf2f6065c0a32166d0b75ac23be79570fa
Wison Liu <31965362+850078897@users.noreply.github.com>
2018-04-26 12:38:20 -0400

    1
    1

--- ChApTeR 12/CLASSWORK.py
+++ ChApTeR 12/CLASSWORK.py
@@ -1,5 +1,10 @@
-myfile =  open("test.txt", "w")
-myfile.write("My first file written from Python\n")
-myfile.write("------------------\n")
-myfile.write("HELLO,WORLD!\n")
-myfile.close()
+import calendar
+cal= calendar.TextCalendar(6)
+
+cal.prmonth(2018,10)
+
+d = calendar.LocaleTextCalendar(6, "klingon")
+d.pryear(2012)
+
+print(calendar.isleap(2022))
+
